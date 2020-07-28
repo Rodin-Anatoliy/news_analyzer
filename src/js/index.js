f7bbbf26b99cb130e3fe8114ccbceaa7c7d20eb6
Anatoliy Rodin <rodin-tolya@mail.ru>
2020-07-29 03:56:34 +0700

    html+css

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,29 @@
 import '../pages/index.css';
 import {about} from './about.js';
 import {paper} from './paper.js';
+
 console.log("index");
 about();
 paper();
+
+
+
+
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  slidesPerView: 3.6,
+  loop: true,
+  spaceBetween: 16,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+})
