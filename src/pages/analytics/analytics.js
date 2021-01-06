17a8572cf60bd676f1721e54df634737a9d744c7
anatoliy <rodin-tolya@mail.ru>
2021-01-06 21:56:42 +0700

    fix analytics page

--- src/pages/analytics/analytics.js
+++ src/pages/analytics/analytics.js
@@ -15,10 +15,9 @@ constants.analyticsNumbers[1].textContent = '';
 constants.titleAnalytics.textContent = '';
 constants.graphicMonth.style.opacity = 0;
 for (let i = 0; i < 7; i++) {
-    constants.daysAccordance[6 - i].classList.remove('animation');
+    constants.daysAccordance[6 - i].classList.remove('showUp');
     constants.daysAccordance[6 - i].textContent = '';
     constants.daysAccordance[6 - i].style.width = `0%`;
-    constants.daysAccordance[6 - i].style.opacity = 0;
     constants.daysWeek[6 - i].style.opacity = 0;
 }
 
@@ -28,29 +27,28 @@ newsApi.getNews(sessionStorage.getItem('input'))
 })
 .then(() => {
     constants.analyticsNumbers[0].textContent = newsStorage.getData().totalResults;
-    constants.analyticsNumbers[0].classList.add('animation');
+    constants.analyticsNumbers[0].classList.add('showUp');
     constants.analyticsNumbers[1].textContent = Math.ceil(accordance(newsStorage.getData().articles, input, 'title') * 0.01 * newsStorage.getData().totalResults);
-    constants.analyticsNumbers[1].classList.add('animation');
+    constants.analyticsNumbers[1].classList.add('showUp');
     constants.titleAnalytics.textContent = `«${sessionStorage.getItem('input')}»`;
-    constants.titleAnalytics.classList.add('animation');
-
-    return Math.ceil(accordance(newsStorage.getData().articles, input, 'title', 'description') * 0.01 * newsStorage.getData().totalResults);
+    constants.titleAnalytics.classList.add('showUp');
+    return constants.analyticsNumbers[1].textContent;
 })
 .then((totalAccordance) => {
     for (let i = 0; i < 7; i++) {
         const day = new Date(Date.parse(today) - i * 86400000);
         constants.daysWeek[6 - i].textContent = `${day.getDate()}, ${constants.week[day.getDay()]}`;
-        constants.daysWeek[6 - i].classList.add('animation');
+        constants.daysWeek[6 - i].classList.add('showUp');
         newsApi.getNews(sessionStorage.getItem('input'), i, i)
         .then((data) => {
+            constants.daysAccordance[6 - i].classList.add('growUp');
             constants.daysAccordance[6 - i].textContent = Math.ceil(accordance(data.articles, input, 'title') * 0.01 * data.totalResults);
             constants.daysAccordance[6 - i].style.width = `${Math.ceil((accordance(data.articles, input, 'title') * 0.01 * data.totalResults) / totalAccordance * 100)}%`;
-            constants.daysAccordance[6 - i].classList.add('animation');
         });
     
     }
     constants.graphicMonth.textContent = `дата (${constants.months[today.getMonth()]})`;
-    constants.graphicMonth.classList.add('animation');
+    constants.graphicMonth.classList.add('showUp');
 })
 .catch(error => {
     constants.loader.style.display = 'none';
